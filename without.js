a373a5b9fc0b74bb33d7a12b653974a51c9a9bf7
Noah Vandenberg <noah@noahvandenberg.com>
2022-01-11 19:27:11 -0800

    created without.js

---
+++ without.js
@@ -0,0 +1,19 @@
+const without = function(source,itemsToRemove) {
+  //console.log('source:', source);
+  //console.log('itemsToRemove:', itemsToRemove);
+  for (let i = 0; i < source.length; i++) {
+    for (let j = 0; j < itemsToRemove.length; j++) {
+      //console.log('source[i]:', source[i], 'itemsToRemove[j]', itemsToRemove[j], 'source[i] === itemsToRemove[j]?:', source[i] === itemsToRemove[j])
+      if (source[i] === itemsToRemove[j]) {
+        //console.log('A:', source.splice(i, 1), 'I:', i)
+        source.splice(i, 1)
+        //i --;
+      }
+    }
+  }
+  return 'ANSWER:' + source
+}
+
+
+console.log(without([1, 2, 3], [1])) // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
