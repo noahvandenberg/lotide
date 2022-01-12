7fa7cf91ada8315899a6f3e637299a2cccfdcf26
Noah Vandenberg <noah@noahvandenberg.com>
2022-01-11 20:18:38 -0800

    created flatten.js

---
+++ flatten.js
@@ -0,0 +1,16 @@
+const flatten = function(input) {
+  let flattenedArray = [];
+  for (let i = 0; i < input.length; i++) {
+    //console.log('For Loop Count:', i, 'input[i][0] value:', input[i][0]);
+    if (input[i][0] === undefined) {
+      flattenedArray.push(input[i]);
+    } else {
+      for (let j = 0; j < input[i].length; j++) {
+        flattenedArray.push(input[i][j]);
+      }
+    }
+  }
+  return flattenedArray;
+};
+
+console.log(flatten([1, 2, [3, 4], 5, [6]]));
