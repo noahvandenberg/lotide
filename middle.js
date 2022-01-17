b922fc4066c3adc1a41cc8a9f2d9b790c99ee124
Noah Vandenberg <noah@noahvandenberg.com>
2022-01-17 09:53:38 -0800

    modulized middle.js

--- middle.js
+++ middle.js
@@ -1,20 +1,4 @@
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1,array2) === false) {
-    console.log('🛑🛑🛑 FAIL: The arrays aren\'t equal')
-  } else if (eqArrays(array1,array2) === true) {
-    console.log('✅✅✅ PASS: The arrays are equal')
-  }
-}
-
-const eqArrays = function(testCaseOne, testCaseTwo) {
-  for (let i = 0; i < testCaseOne.length; i++) {
-    //console.log('type 1:',typeof testCaseOne[i], 'type 2:', typeof testCaseTwo[i]) // Type Checker
-    if (testCaseOne[i] !== testCaseTwo[i] || typeof testCaseOne[i] !== typeof testCaseTwo[i]) {
-      return false
-    } 
-  }
-  return true
-}
+const assertArraysEqual = require('./assertArraysEqual');
 
 const middle = function(input) {
   //console.log('Input Length:', input.length)
@@ -29,17 +13,5 @@ const middle = function(input) {
   };
 };
 
-// middle([1]) // => []
-// middle([1, 2]) // => []
-// middle([1, 2, 3]) // => [2]
-// middle([1, 2, 3, 4, 5]) // => [3]
-// middle([1, 2, 3, 4]) // => [2, 3]
-// middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
-
-assertArraysEqual(middle([1]),[])
-assertArraysEqual(middle([1, 2]),[])
-assertArraysEqual(middle([1, 2, 3]),[2])
-assertArraysEqual(middle([1, 2, 3, 4, 5]),[3])
-assertArraysEqual(middle([1, 2, 3, 4]),[2, 3])
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3, 4])
+module.exports = middle;
 
