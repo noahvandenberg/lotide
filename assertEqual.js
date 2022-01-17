696bcb63d648bf274f2e936659edba7a5749d6eb
Noah Vandenberg <noah@noahvandenberg.com>
2022-01-17 09:24:00 -0800

    removed test code from assertEqual.js

--- assertEqual.js
+++ assertEqual.js
@@ -10,5 +10,5 @@ const assertEqual = function(actual, expected) {
 module.exports = assertEqual;
 
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+//assertEqual("Lighthouse Labs", "Bootcamp");
+//assertEqual(1, 1);
