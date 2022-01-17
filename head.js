45b191be9a84f3387b57b319c76c7cd5eaf61630
Noah Vandenberg <noah@noahvandenberg.com>
2022-01-17 09:27:45 -0800

    commented out test code from head.js

--- head.js
+++ head.js
@@ -7,5 +7,5 @@ const head = function(array) {
 module.exports = head;
 
 // Tests
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+//assertEqual(head([5,6,7]), 5);
+//assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
