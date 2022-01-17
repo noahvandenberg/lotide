70462a4a4117a7f675e588b45bf81b8228879334
Noah Vandenberg <noah@noahvandenberg.com>
2022-01-17 10:10:07 -0800

    changed assertEqual()

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,10 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
+    return true
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
+    return false
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
