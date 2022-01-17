8bee4d1fb71baeebe555bfddd95a7c8f1a8b71f5
Noah Vandenberg <noah@noahvandenberg.com>
2022-01-17 09:43:36 -0800

    modulized eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
+const assertEqual = require('./assertEqual')
 
 const eqArrays = function(testCaseOne, testCaseTwo) {
   for (let i = 0; i < testCaseOne.length; i++) {
@@ -18,9 +11,4 @@ const eqArrays = function(testCaseOne, testCaseTwo) {
   return true
 }
 
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]),true) // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]),false) // => false
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]),true) // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]),false) // => false
+module.exports = eqArrays
