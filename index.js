4833b96977f2ab7f6e8b47007e7e6749fd147a48
Noah Vandenberg <noah@noahvandenberg.com>
2022-01-17 13:33:19 -0800

    created index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+const assertArraysEqual = require('./assertArraysEqual')
+const assertEqual = require('./assertEqual')
+const assertObjectsEqual = require('./assertObjectsEqual')
+const countLetters = require('./countLetters')
+const countOnly = require('./countOnly')
+const eqArrays = require('./eqArrays')
+const eqObjects = require('./eqObjects')
+const findKey = require('./findKey')
+const findKeyByValue = require('./findKeyByValue')
+const flatten = require('./flatten')
+const head   = require('./head');
+const letterPositions = require('./letterPositions')
+const map = require('./map')
+const tail   = require('./tail');
+const middle = require('./middle');
+const takeUntil = require('./takeUntil')
+const without = require('./without')
+
+module.exports = {
+  assertArraysEqual : assertArraysEqual,
+  assertEqual : assertEqual,
+  assertObjectsEqual : assertObjectsEqual,
+  countLetters : countLetters,
+  countOnly : countOnly,
+  eqArrays : eqArrays,
+  eqObjects : eqObjects,
+  findKey : findKey,
+  findKeyByValue : findKeyByValue,
+  flatten: flatten,
+  head : head,
+  letterPositions : letterPositions,
+  map : map,
+  tail : tail,
+  middle : middle,
+  takeUntil : takeUntil,
+  without : without
+};
