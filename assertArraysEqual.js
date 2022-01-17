ff11acbaee0ae19d727e865624834ffc8791641c
Noah Vandenberg <noah@noahvandenberg.com>
2022-01-17 10:06:39 -0800

    changed assertArraysEqual()

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -2,8 +2,10 @@ const eqArrays = require('./eqArrays')
 
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1,array2) === false) {
+    return false
     console.log('🛑🛑🛑 FAIL: The arrays aren\'t equal')
   } else if (eqArrays(array1,array2) === true) {
+    return true
     console.log('✅✅✅ PASS: The arrays are equal')
   }
 }
